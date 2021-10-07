61526e33b668500fef6818a34b180dc5e15e2ead
Miguel de Castro <92113852+nascimento8@users.noreply.github.com>
2021-10-07 14:41:43 -0300

    Added subtraction operation

--- calculator.py
+++ calculator.py
@@ -3,4 +3,14 @@ x=input()
 print("Digite o valor de Y:")
 y=input()
 
-print("A soma de X + Y = {}".format(int(x)+int(y)))
+print("Digite a operacao:")
+operation=input()
+
+if (operation == "soma"):
+    result=int(x)+int(y)
+    print("O resultado e = {}".format(result))
+elif (operation == "subtracao"):
+    result=int(x)-int(y)
+    print("O resultado e = {}".format(result))
+else:
+    print("Operacao '{}' nao reconhecida".format(operation))
